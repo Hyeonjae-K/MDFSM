032d479b4f229176055b310f736540d7653b3706
nowzero1702 <109930745+nowzero1702@users.noreply.github.com>
2022-08-08 02:07:36 +0900

    Update spline_ver0.2.cs

--- Assets/trajectory/spline_ver0.2.cs
+++ Assets/trajectory/spline_ver0.2.cs
@@ -61,6 +61,8 @@ public class spline : MonoBehaviour {
 	{
 		get { updatePointsList(); return waypointList.items; }
 	}
+	
+// waypoint 등록시 position으로 자동 갱신 및 마지막 점을 초기점으로 초기화.
 	void updatePointsList()
 	{
 		var children = new Transform[transform.childCount];
@@ -131,12 +133,15 @@ public class spline : MonoBehaviour {
 		}
 		editorVisualisationSubsteps = (int) distances[Waypoints.Length];
 	}
+	
+	// mathplus bezier, catmullrome spline 알고리즘 원형 코드
 	private Vector3 CatmullRom(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float i)
 	{
 		return 0.5f*
 		       ((2*p1) + (-p0 + p2)*i + (2*p0 - 5*p1 + 4*p2 - p3)*i*i +
 		        (-p0 + 3*p1 - 3*p2 + p3)*i*i*i);
 	}
+	
 	public Vector3 GetRoutePosition(float dist)
 	{
 		int point = 0;
@@ -324,167 +329,3 @@ public class spline : MonoBehaviour {
 		// Debug.Log(point [i].x, point [i].y, point [i].z);
 	}
 }
-// namespace UnityStandardAssets.Utility.Inspector
-// {
-// #if UNITY_EDITOR
-//     [CustomPropertyDrawer(typeof(WaypointCircuit.WaypointList))]
-//     public class WaypointListDrawer : PropertyDrawer
-//     {
-//         private float lineHeight = 18;
-//         private float spacing = 4;
-//
-//
-//         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-//         {
-//             EditorGUI.BeginProperty(position, label, property);
-//
-//             float x = position.x;
-//             float y = position.y;
-//             float inspectorWidth = position.width;
-//
-//             // Draw label
-//
-//
-//             // Don't make child fields be indented
-//             var indent = EditorGUI.indentLevel;
-//             EditorGUI.indentLevel = 0;
-//
-//             var items = property.FindPropertyRelative("items");
-//             var titles = new string[] { "Transform", "", "", "" };
-//             var props = new string[] { "transform", "^", "v", "-" };
-//             var widths = new float[] { .7f, .1f, .1f, .1f };
-//             float lineHeight = 18;
-//             bool changedLength = false;
-//             if (items.arraySize > 0)
-//             {
-//                 for (int i = -1; i < items.arraySize; ++i)
-//                 {
-//                     var item = items.GetArrayElementAtIndex(i);
-//
-//                     float rowX = x;
-//                     for (int n = 0; n < props.Length; ++n)
-//                     {
-//                         float w = widths[n] * inspectorWidth;
-//
-//                         // Calculate rects
-//                         Rect rect = new Rect(rowX, y, w, lineHeight);
-//                         rowX += w;
-//
-//                         if (i == -1)
-//                         {
-//                             EditorGUI.LabelField(rect, titles[n]);
-//                         }
-//                         else
-//                         {
-//                             if (n == 0)
-//                             {
-//                                 EditorGUI.ObjectField(rect, item.objectReferenceValue, typeof(Transform), true);
-//                             }
-//                             else
-//                             {
-//                                 if (GUI.Button(rect, props[n]))
-//                                 {
-//                                     switch (props[n])
-//                                     {
-//                                         case "-":
-//                                             items.DeleteArrayElementAtIndex(i);
-//                                             items.DeleteArrayElementAtIndex(i);
-//                                             changedLength = true;
-//                                             break;
-//                                         case "v":
-//                                             if (i > 0)
-//                                             {
-//                                                 items.MoveArrayElement(i, i + 1);
-//                                             }
-//                                             break;
-//                                         case "^":
-//                                             if (i < items.arraySize - 1)
-//                                             {
-//                                                 items.MoveArrayElement(i, i - 1);
-//                                             }
-//                                             break;
-//                                     }
-//                                 }
-//                             }
-//                         }
-//                     }
-//
-//                     y += lineHeight + spacing;
-//                     if (changedLength)
-//                     {
-//                         break;
-//                     }
-//                 }
-//             }
-//             else
-//             {
-//                 // add button
-//                 var addButtonRect = new Rect((x + position.width) - widths[widths.Length - 1] * inspectorWidth, y,
-//                                                 widths[widths.Length - 1] * inspectorWidth, lineHeight);
-//                 if (GUI.Button(addButtonRect, "+"))
-//                 {
-//                     items.InsertArrayElementAtIndex(items.arraySize);
-//                 }
-//
-//                 y += lineHeight + spacing;
-//             }
-//
-//             // add all button
-//             var addAllButtonRect = new Rect(x, y, inspectorWidth, lineHeight);
-//             if (GUI.Button(addAllButtonRect, "Assign using all child objects"))
-//             {
-//                 var circuit = property.FindPropertyRelative("circuit").objectReferenceValue as WaypointCircuit;
-//                 var children = new Transform[circuit.transform.childCount];
-//                 int n = 0;
-//                 foreach (Transform child in circuit.transform)
-//                 {
-//                     children[n++] = child;
-//                 }
-//                 Array.Sort(children, new TransformNameComparer());
-//                 circuit.waypointList.items = new Transform[children.Length];
-//                 for (n = 0; n < children.Length; ++n)
-//                 {
-//                     circuit.waypointList.items[n] = children[n];
-//                 }
-//             }
-//             y += lineHeight + spacing;
-//
-//             // rename all button
-//             var renameButtonRect = new Rect(x, y, inspectorWidth, lineHeight);
-//             if (GUI.Button(renameButtonRect, "Auto Rename numerically from this order"))
-//             {
-//                 var circuit = property.FindPropertyRelative("circuit").objectReferenceValue as WaypointCircuit;
-//                 int n = 0;
-//                 foreach (Transform child in circuit.waypointList.items)
-//                 {
-//                     child.name = "Waypoint " + (n++).ToString("000");
-//                 }
-//             }
-//             y += lineHeight + spacing;
-//
-//             // Set indent back to what it was
-//             EditorGUI.indentLevel = indent;
-//             EditorGUI.EndProperty();
-//         }
-//
-//
-//         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-//         {
-//             SerializedProperty items = property.FindPropertyRelative("items");
-//             float lineAndSpace = lineHeight + spacing;
-//             return 40 + (items.arraySize * lineAndSpace) + lineAndSpace;
-//         }
-//
-//
-//         // comparer for check distances in ray cast hits
-//         public class TransformNameComparer : IComparer
-//         {
-//             public int Compare(object x, object y)
-//             {
-//                 return ((Transform)x).name.CompareTo(((Transform)y).name);
-//             }
-//         }
-//     }
-//
-// #endif
-// }
